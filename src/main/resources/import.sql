aa0f824bc2254fbd8e87ab8181acc977ee0d838f
wilianAlbrecht <wilian0012@hotmail.com>
2023-10-09 00:17:43 -0400

    adição dos primeiros campos utilizando o h2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,23 @@
+INSERT INTO disciplina VALUES (1, 'ementa', 'geografia')
+
+INSERT INTO professor VALUES (1, 1, '1975-05-25', 'professor' ,'rodrigo', 'rodrigo', '123')
+
+INSERT INTO turma VALUES (1, 1, 1)
+
+INSERT INTO aluno VALUES (1, 1, 1,'1999-12-27', 'wilian', 'wilian', '123')
+INSERT INTO aluno VALUES (2, 1, 1,'1998-09-05', 'maria', 'maria', '123')
+INSERT INTO aluno VALUES (3, 1, 1,'1988-11-08', 'paulo', 'paulo', '123')
+
+-- INSERT INTO trabalho VALUES (1, 1,'trabalho de geografia')
+
+-- INSERT INTO rendimento_escolar VALUES (1, 1, 5.5, 5.0, 5, 6 , 1)
+-- INSERT INTO rendimento_escolar VALUES (2, 2, 10, 5.0, 5, 6 , 1)
+-- INSERT INTO rendimento_escolar VALUES (3, 3, 7.5, 5.0, 5, 6 , 1)
+
+-- INSERT INTO trabalho_nota VALUES (1, 1, 1,1)
+-- INSERT INTO trabalho_nota VALUES (2, 2, 2,1)
+-- INSERT INTO trabalho_nota VALUES (3, 3, 3,1)
+
+-- INSERT INTO RENDIMENTO_ESCOLAR_TRABALHO_NOTA VALUES (1,1)
+-- INSERT INTO RENDIMENTO_ESCOLAR_TRABALHO_NOTA VALUES (2,2)
+-- INSERT INTO RENDIMENTO_ESCOLAR_TRABALHO_NOTA VALUES (3,3)
